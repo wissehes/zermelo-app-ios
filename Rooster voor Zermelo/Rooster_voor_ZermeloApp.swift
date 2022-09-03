9e11c8a136114c2179b364d1dc0086085b2aeb36
Wisse <wissehes@gmail.com>
2022-09-03 17:24:59 +0200

    Initial Commit

---
+++ Rooster voor Zermelo/Rooster_voor_ZermeloApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rooster_voor_ZermeloApp.swift
+//  Rooster voor Zermelo
+//
+//  Created by Wisse Hes on 03/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Rooster_voor_ZermeloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
