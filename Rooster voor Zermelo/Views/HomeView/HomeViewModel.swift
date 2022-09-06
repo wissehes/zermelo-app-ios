54eb648320b228b4df88bd5fee639493749e8a5c
Wisse <wissehes@gmail.com>
2022-09-06 12:23:16 +0200

    Organize appointments by day, for a future week view

--- Rooster voor Zermelo/Views/HomeView/HomeViewModel.swift
+++ Rooster voor Zermelo/Views/HomeView/HomeViewModel.swift
@@ -8,9 +8,16 @@
 import Foundation
 import Alamofire
 
+struct Day {
+    let date: Date
+    var appointments: [ZermeloLivescheduleAppointment]
+}
+
 final class HomeViewModel: ObservableObject {
     @Published var todayAppointments: [ZermeloLivescheduleAppointment] = []
     @Published var isLoading = true
+    
+    @Published var days: [Day] = []
     
     @Published var appointmentDetailsShown = false
     @Published var selectedAppointment: ZermeloLivescheduleAppointment?
@@ -28,6 +35,23 @@ final class HomeViewModel: ObservableObject {
             self.isLoading = false
             switch result {
             case .success(let data):
+                
+                for appointment in data {
+                    let date = Date(timeIntervalSince1970: TimeInterval(appointment.start))
+                    
+                    let foundRow = self.days.firstIndex { day in
+                        Calendar.current.isDate(day.date, equalTo: date, toGranularity: .day)
+                    }
+                    
+                    if let foundRow = foundRow {
+                        self.days[foundRow].appointments.append(appointment)
+                    } else {
+                        self.days.append(Day( date: date, appointments: [appointment] ))
+                    }
+                }
+                
+                print(self.days.map { $0.appointments.map { $0.subjects } })
+                
                 self.todayAppointments = data.filter {
                     Calendar.current.isDateInToday(Date(timeIntervalSince1970: TimeInterval($0.start)))
                 }
