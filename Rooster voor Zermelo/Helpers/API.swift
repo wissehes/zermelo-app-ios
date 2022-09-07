3cc0251d52de6fdb62af22fc8f6ef3da3ee58078
Wisse <wissehes@gmail.com>
2022-09-07 10:45:53 +0200

    Creat an async method for fetching the liveschedule

--- Rooster voor Zermelo/Helpers/API.swift
+++ Rooster voor Zermelo/Helpers/API.swift
@@ -34,4 +34,25 @@ final class API {
                 }
             }
     }
+    
+    static func getLiveScheduleAsync(me: ZermeloMeData, week: String = "202236") async throws -> [ZermeloLivescheduleAppointment] {
+        guard let token = TokenSaver.get() else { fatalError("No token") }
+        
+        guard var url = URLComponents(string: "https://\(token.portal).zportal.nl/api/v3/liveschedule") else { fatalError("url error") }
+        url.queryItems = [
+            URLQueryItem(name: "student", value: me.code),
+            URLQueryItem(name: "week", value: week)
+        ]
+        
+        var urlRequest = URLRequest(url: url.url!)
+        urlRequest.addValue("Bearer \(token.access_token)", forHTTPHeaderField: "Authorization")
+        
+        let (data, _) = try await URLSession.shared.data(for: urlRequest)
+        
+        if let decoded = try? JSONDecoder().decode(GetZermeloLiveschedule.self, from: data) {
+            if let data = decoded.response.data.first {
+                return data.appointments
+            } else { return [] }
+        } else { return [] }
+    }
 }
