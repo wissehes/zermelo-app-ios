d33bccef6a56bdfd31c27affa4bddd9f7d63d0e2
Wisse <wissehes@gmail.com>
2023-01-13 09:44:37 +0100

    Improve newWeekView (WiP)

--- Rooster voor Zermelo/Views/WeekView/NewWeekView.swift
+++ Rooster voor Zermelo/Views/WeekView/NewWeekView.swift
@@ -13,36 +13,51 @@ struct NewWeekView: View {
     let a = ["GS", "DUTL", "NETL", "WA"]
     let l = ["6s", "10r", "11a", "at1-bov", "18l"]
     
+    @StateObject var viewModel = WeekViewModel()
+    @EnvironmentObject var authManager: AuthManager
+
+    
     var body: some View {
-        HStack(alignment: .top) {
-            ForEach(days, id: \.self) { day in
-                VStack(alignment: .center) {
-                    Text(day)
-                        .fontWeight(.heavy)
-                        .foregroundColor(.secondary)
-                    
-                    ForEach(1...5, id: \.self) { _ in
-                        appointment()
+        GeometryReader { geo in
+            HStack(alignment: .top) {
+                Spacer()
+                ForEach(viewModel.days, id: \.date.timeIntervalSince1970) { day in
+                    VStack(alignment: .center) {
+                        Text(day.shortDay)
+                            .fontWeight(.heavy)
+                            .foregroundColor(.secondary)
+                        //                        .multilineTextAlignment(.leading)
+                        
+                        ForEach(day.appointments, id: \.self) { app in
+                            appointment(geo, app: app)
+                        }
+                        
+                        Spacer()
                     }
-                    
-                    Spacer()
                 }
+                Spacer()
             }
         }.navigationTitle("Week")
-        //            .padding(0)
+            .task {
+                guard let me = authManager.me else { return }
+                await viewModel.load(me: me, date: nil)
+            }
     }
     
-    func appointment() -> some View {
-        VStack(alignment: .leading) {
-            Text(a.randomElement()!)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.leading)
-            
-            Text(l.randomElement()!)
-                .foregroundColor(.secondary)
-                .multilineTextAlignment(.leading)
-            
-        }.frame(width: 70, height: 50)
+    func appointment(_ geo: GeometryProxy, app: ZermeloLivescheduleAppointment) -> some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(app.subjects.joined(separator: ","))
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.leading)
+                
+                Text(app.locations.joined(separator: ","))
+                    .foregroundColor(.secondary)
+                    .multilineTextAlignment(.leading)
+                
+            }.padding(5)
+            Spacer()
+        }.frame(width: (geo.size.width - 50) / 5, height: 60)
             .overlay(
                 RoundedRectangle(cornerRadius: 2.5)
                     .stroke(lineWidth: 1)
@@ -55,6 +70,6 @@ struct NewWeekView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
             NewWeekView()
-        }
+        }.environmentObject(AuthManager())
     }
 }
