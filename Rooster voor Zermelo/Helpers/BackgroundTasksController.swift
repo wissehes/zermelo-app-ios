b800d4f37ad8245cbb80ce3ce8db3297639bf41d
Wisse <wissehes@gmail.com>
2023-01-24 22:00:08 +0100

    Create BackgroundTasksController

--- Rooster voor Zermelo/Helpers/BackgroundTasksController.swift
+++ Rooster voor Zermelo/Helpers/BackgroundTasksController.swift
@@ -12,7 +12,16 @@ final class BackgroundTasksController {
     static func scheduleAppRefresh() {
         let request = BGAppRefreshTaskRequest(identifier: "notificationrefresh")
         // Add 12 hours
-        request.earliestBeginDate = .now.addingTimeInterval(12 * 3600)
-        try? BGTaskScheduler.shared.submit(request)
+//        request.earliestBeginDate = .now.addingTimeInterval(12 * 3600)
+        // add 24 hours and set the time to 1 am
+        let date = Date.now.addingTimeInterval(24 * 3600)
+        request.earliestBeginDate = Calendar.current.date(bySettingHour: 1, minute: 00, second: 00, of: date)
+
+        
+        do {
+            try BGTaskScheduler.shared.submit(request)
+        } catch(let err) {
+            print(err)
+        }
     }
 }
